a65b2367ee254b43e3c40dad1793d17c50fc664c
Ian Stride <ianstride@gmail.com>
2023-11-08 23:36:54 +0000

    Introduce script to transcode videos to ParentText specification (#31)

---
+++ src/parenttext/transcode.py
@@ -0,0 +1,139 @@
+import argparse
+import subprocess
+import shutil
+from pathlib import Path
+
+
+def start():
+    args = parse_args()
+    transcode(args.source, prepare(args.destination, wipe=True), args.format)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=("Transcode videos to ParentText specification"),
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "source",
+        help=("directory containing source video files"),
+    )
+    parser.add_argument(
+        "destination",
+        help=("directory where transcoded videos will be saved"),
+    )
+    parser.add_argument(
+        "-f",
+        "--format",
+        default="video",
+        choices=["audio", "video"],
+        help=("output format (default: 'video')"),
+    )
+
+    return parser.parse_args()
+
+
+def prepare(dst, wipe=False):
+    dest = Path(dst)
+
+    if wipe and dest.exists():
+        shutil.rmtree(dest)
+        print("Directory deleted,", {"path": str(dest)})
+
+    if not dest.exists():
+        dest.mkdir(parents=True)
+        print("Directory created,", {"path": str(dest)})
+
+    return dst
+
+
+def transcode(src, dst, fmt="video"):
+    src_root = Path(src)
+    dst_root = Path(dst)
+    op = {"video": to_video, "audio": to_audio}[fmt]
+    sources = list(src_root.glob("**/*.mp4"))
+    count = len(sources)
+
+    print("Source files found,", {"count": count})
+
+    for i, video_src in enumerate(sources, start=1):
+        file_dst = dst_root / video_src.relative_to(src_root)
+        prepare(file_dst.parent)
+        final_dst = op(video_src, file_dst)
+        print(
+            "Operation completed,",
+            {
+                "op": op.__name__,
+                "source": str(video_src),
+                "dest": str(final_dst),
+                "progress": f"{i}/{count}",
+            },
+        )
+
+
+def to_video(src: Path, dst: Path):
+    first_pass(src)
+    second_pass(src, dst)
+
+    return dst
+
+
+def to_audio(src: Path, dst: Path):
+    final_dst = dst.with_suffix(".mp3")
+    subprocess.run(
+        ["ffmpeg"]
+        + ["-y"]
+        + ["-loglevel", "warning"]
+        + ["-i", str(src)]
+        + ["-c:a", "mp3"]
+        + ["-b:a", "64k"]
+        + ["-ar", "48k"]
+        + ["-ac", "1"]
+        + ["-f", "mp3"]
+        + [str(final_dst)],
+    )
+
+    return final_dst
+
+
+def first_pass(src):
+    subprocess.run(
+        ["ffmpeg"]
+        + ["-y"]
+        + ["-loglevel", "warning"]
+        + ["-i", str(src)]
+        + ["-c:v", "libx264"]
+        + ["-r:v", "20"]
+        + ["-s:v", "720x406"]
+        + ["-b:v", "84k"]
+        + ["-profile:v", "baseline"]
+        + ["-pass", "1"]
+        + ["-an"]
+        + ["-f", "null"]
+        + ["/dev/null"],
+    )
+
+
+def second_pass(src, dst):
+    subprocess.run(
+        ["ffmpeg"]
+        + ["-y"]
+        + ["-loglevel", "warning"]
+        + ["-i", str(src)]
+        + ["-c:v", "libx264"]
+        + ["-r:v", "20"]
+        + ["-s:v", "720x406"]
+        + ["-b:v", "84k"]
+        + ["-profile:v", "baseline"]
+        + ["-pass", "2"]
+        + ["-c:a", "aac"]
+        + ["-b:a", "42k"]
+        + ["-ar", "24k"]
+        + ["-ac", "1"]
+        + ["-f", "mp4"]
+        + [str(dst)],
+    )
+
+
+if __name__ == "__main__":
+    start()

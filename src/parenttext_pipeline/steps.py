5920e7cecc6befc015405ee3641f9eb5335b57f6
Ed Moss <ed.moss@eemengineering.com>
2024-06-13 00:20:09 +0100

    Including new QR function

--- src/parenttext_pipeline/steps.py
+++ src/parenttext_pipeline/steps.py
@@ -129,6 +129,18 @@ def apply_qr_treatment(config, step_config, step_number, step_input_file):
             special_words_file,
         )
         print("Step 8 complete, reformatted quick replies")
+    elif step_config.qr_treatment == "reformat_whatsapp":
+        run_node(
+            "idems_translation_chatbot/index.js",
+            "reformat_quick_replies_whatsapp",
+            step_input_file,
+            select_phrases_file,
+            step_output_basename,
+            config.temppath,
+            str(step_config.qr_limit),
+            special_words_file,
+        )
+        print("Step 8 complete, reformatted quick replies")    
     elif step_config.qr_treatment == "reformat_china":
         run_node(
             "idems_translation_chatbot/index.js",
